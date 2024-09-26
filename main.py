3e5749b30ebdf22a0733205e144066181f1059a8
martabritala <53839047+martabritala@users.noreply.github.com>
2024-09-26 16:49:11 +0300

    26.09. stundas darbs

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from cilveks import Cilveks, Sieviete
 import tkinter as tk
-from tkinter import ttk
+from tkinter import ttk, END
 
 #cilvēku saraksts
 
@@ -46,6 +46,13 @@ vecums = tk.IntVar()
 vecums_entry = ttk.Entry(frame, textvariable=vecums)
 vecums_entry.grid(column=1, row=2, **options)
 
+# Listbox saraksta atjaunošana
+def nomainit_sarakstu():
+    listbox.delete(0,END)
+    for cilveks in visi_cilveki:
+        listbox.insert("end","{},{},{}".format(cilveks.vards,cilveks.dzimums,cilveks.vecums))
+
+
 # ražošana button
 
 def razot_button_clicked():
@@ -54,6 +61,8 @@ def razot_button_clicked():
     cilveka_vecums = vecums.get()
     visi_cilveki.append(Cilveks(cilveka_vards,cilveka_dzimums,cilveka_vecums))
     result_label.config(text=visi_cilveki[-1].info())
+    nomainit_sarakstu()
+    # listbox.insert("end","{},{},{}".format(cilveka_vards,cilveka_dzimums,cilveka_vecums))
 
 
 
@@ -61,6 +70,30 @@ razot_button = ttk.Button(frame, text='Ražot')
 razot_button.grid(column=2, row=0, sticky='W', **options)
 razot_button.configure(command=razot_button_clicked)
 
+def dzimsanas_diena():
+    jaunais_teksts = ""
+    for izveletais in listbox.curselection():
+        visi_cilveki[izveletais].svinet_dz_d()
+        jaunais_teksts += visi_cilveki[izveletais].info() + "\n"
+    result_label.config(text=jaunais_teksts)
+    nomainit_sarakstu()
+
+
+
+dzimene_button = ttk.Button(frame, text='Dzimšanas diena!')
+dzimene_button.grid(column=4, row=2, sticky='W', **options)
+dzimene_button.configure(command=dzimsanas_diena)
+
+saturs = tk.Variable(value=tuple(visi_cilveki))
+
+listbox = tk.Listbox(
+    root,
+    listvariable=saturs,
+    height=6,
+    selectmode=tk.EXTENDED
+)
+
+listbox.grid(row=4, columnspan=3, **options)
 
 # result label
 result_label = ttk.Label(frame)
