6decbbc365747bc6bc227a7456283dde52b178ff
martabritala <53839047+martabritala@users.noreply.github.com>
2024-10-10 17:02:07 +0300

    10.10.2024

---
+++ datubazes/datubaze.py
@@ -0,0 +1,37 @@
+import sqlite3
+
+conn = sqlite3.connect("datubazes/faili/my.db")
+
+def kverijs(vaicajums):
+    cur = conn.cursor()
+    cur.execute(vaicajums)
+    conn.commit()
+
+
+
+tabulas_dzesana = "DROP TABLE skoleni"
+
+tabulas_izveide = """
+CREATE TABLE IF NOT EXISTS skoleni(
+    id_skolenam INTEGER PRIMARY KEY AUTOINCREMENT,
+    vards TEXT,
+    uzvards TEXT,
+    vecums INTEGER
+)
+
+"""
+datu_pievienosana = """
+INSERT INTO skoleni (vards, uzvards, vecums)
+VALUES('Anna', 'Bērziņa', 18)
+"""
+# kverijs(datu_pievienosana)
+
+
+def datu_pieliksana(tabula, kolonnas, dati):
+    vaicajums = f"""
+INSERT INTO {tabula} {kolonnas}
+VALUES {dati}
+"""
+    kverijs(vaicajums)
+
+# datu_pieliksana("skoleni",("vards", "uzvards", "vecums"), ("Trešais","Bērns", 18))
